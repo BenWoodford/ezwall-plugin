0b579a3b1c6725f29dbae5bced679d76d9a663b7
Cue <cue@loadingchunks.net>
2013-09-04 18:20:21 +0100

    New webapp stuff!

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,22 +1,33 @@
-console.log('EZWall starting...');
-require.config({
-	paths: {
-		jquery: 'libs/jquery-1.7.2.min',
-		jqueryui: 'libs/jquery-ui-1.8.18.min',
-		jquerytmpl: 'libs/jquery.tmpl.min',
-		underscore: 'libs/underscore-amd-min',
-		backbone: 'libs/backbone-0.9.1-amd-min',
-		md5: 'libs/spark-md5.amd'
-	}
-});
+$(document).ready(function() {
+    $.getJSON("/api/json", function(json, textStatus) {
+        if(json.jobs.length > 0) {
+            $.each(json.jobs, function(index, val) {
+                if($("#dashboard #" + val.name).length == 0) {
+                    $("#dashboard").add('<div class="alert span3" id="' + val.name + '"><h2 class="job-title">' + val.name + '</h2><p class="job-status">' + val.status + '</p></div>');
+                }
 
-require([
-	'order!jquery',
-	'order!underscore',
-	'order!backbone',
-	'order!jquerytmpl',
-	'order!jqueryui',
-	'order!app'
-], function($, _, backbone, jqueryTmpl, jqueryUi, App){
-	App.start();
+                var job = $("#dashboard #" + val.name);
+
+                switch(val.color) {
+                    case 'notbuilt':
+                    case 'grey':
+                    case 'disabled':
+                        $(job).addClass('alert-info');
+                        break;
+
+                    case 'blue':
+                        $(job).addClass('alert-success');
+                        break;
+
+                    case 'red':
+                        $(job).addClass('alert-error');
+                        break;
+
+                    case 'yellow':
+                        $(job).addClass('alert-warning');
+                        break;
+                }
+            });
+        }
+    });
 });
