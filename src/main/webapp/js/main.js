1022ce2880ed42cf923749bd060f91205f5a864e
Cue <cue@loadingchunks.net>
2013-09-04 23:50:34 +0100

    Get jobs every 5 seconds

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,53 +1,56 @@
 $(document).ready(function() {
-    $.getJSON("/api/json", function(json, textStatus) {
-        if(json.jobs.length > 0) {
-            $.each(json.jobs, function(index, val) {
-                if(val.color == "notbuilt")
-                    return true;
+        function getJobs() {
+            $.getJSON("/api/json", function(json, textStatus) {
+                if(json.jobs.length > 0) {
+                    $.each(json.jobs, function(index, val) {
+                        if(val.color == "notbuilt")
+                            return true;
 
-                if($("#dashboard #" + val.name).length == 0) {
-                    $("#dashboard").append('<div class="job col-md-3"><div class="job-inner alert" id="' + val.name + '"><h2 class="job-title">' + val.name + '</h2><p class="job-status"></p></div></div>');
-                }
+                        if($("#dashboard #" + val.name).length == 0) {
+                            $("#dashboard").append('<div class="job col-md-3"><div class="job-inner alert" id="' + val.name + '"><h2 class="job-title">' + val.name + '</h2><p class="job-status"></p></div></div>');
+                        }
 
-                var job = $("#dashboard #" + val.name);
+                        var job = $("#dashboard #" + val.name);
 
-                switch(val.color) {
-                    case 'blue_anime':
-                        $(job).addClass('alert-info', 'fast');
-                        $(job).find('.job-status').text("Building from Stable...");
-                        break;
+                        switch(val.color) {
+                            case 'blue_anime':
+                                $(job).addClass('alert-info', 'fast');
+                                $(job).find('.job-status').text("Building from Stable...");
+                                break;
 
-                    case 'red_anime':
-                        $(job).addClass('alert-info', 'fast');
-                        $(job).find('.job-status').text("Building from Failure...");
-                        break;
+                            case 'red_anime':
+                                $(job).addClass('alert-info', 'fast');
+                                $(job).find('.job-status').text("Building from Failure...");
+                                break;
 
-                    case 'yellow_anime':
-                        $(job).addClass('alert-info', 'fast');
-                        $(job).find('.job-status').text("Building from Unstable...");
-                        break;
+                            case 'yellow_anime':
+                                $(job).addClass('alert-info', 'fast');
+                                $(job).find('.job-status').text("Building from Unstable...");
+                                break;
 
-                    case 'blue':
-                        $(job).addClass('alert-success', 'fast');
-                        $(job).find('.job-status').text("Stable Build!");
-                        break;
+                            case 'blue':
+                                $(job).addClass('alert-success', 'fast');
+                                $(job).find('.job-status').text("Stable Build!");
+                                break;
 
-                    case 'red':
-                        $(job).addClass('alert-danger', 'fast');
-                        $(job).find('.job-status').text("Build Failed");
-                        break;
+                            case 'red':
+                                $(job).addClass('alert-danger', 'fast');
+                                $(job).find('.job-status').text("Build Failed");
+                                break;
 
-                    case 'yellow':
-                        $(job).addClass('alert-warning', 'fast');
-                        $(job).find('.job-status').text("Build Unstable");
-                        break;
+                            case 'yellow':
+                                $(job).addClass('alert-warning', 'fast');
+                                $(job).find('.job-status').text("Build Unstable");
+                                break;
 
-                    default:
-                        $(job).addClass('alert-warning', 'fast');
-                        $(job).find('.job-status').text("Unknown Status Type: " + val.color);
-                        break;
+                            default:
+                                $(job).addClass('alert-warning', 'fast');
+                                $(job).find('.job-status').text("Unknown Status Type: " + val.color);
+                                break;
+                        }
+                    });
                 }
             });
         }
-    });
+    setInterval("getJobs()", 5000);
 });
