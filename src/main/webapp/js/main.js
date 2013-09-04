471447284a45e9a97f0976bde1699c9dfda32bc2
Cue <cue@loadingchunks.net>
2013-09-04 22:51:18 +0100

    Don't show unbuilt ones.

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     $.getJSON("/api/json", function(json, textStatus) {
         if(json.jobs.length > 0) {
             $.each(json.jobs, function(index, val) {
+                if(val.color == "notbuilt")
+                    continue;
+
                 if($("#dashboard #" + val.name).length == 0) {
                     $("#dashboard").append('<div class="job col-md-3"><div class="job-inner alert" id="' + val.name + '"><h2 class="job-title">' + val.name + '</h2><p class="job-status">' + val.color + '</p></div></div>');
                 }
